c0e22c2b3b4e736e88a73ef0ca4668c4cd8d8d93
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2024-09-05 00:29:20 +0300

    Update f1.user.js

--- f1.user.js
+++ f1.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         F1_
+// @name         F11
 // @namespace    f11
 // @version      3.0
-// @description  try
+// @description  tryyy
 // @author       el4upacabr
 // @match        https://www.bing.com/search?q=%d1%81%d0%bf%d1%80%d0%b0%d0%b2%d0%ba%d0%b0+%d0%be%d0%b1+%d0%b8%d1%81%d0%bf%d0%be%d0%bb%d1%8c%d0%b7%d0%be%d0%b2%d0%b0%d0%bd%d0%b8%d0%b8+%d0%bf%d1%80%d0%be%d0%b2%d0%be%d0%b4%d0%bd%d0%b8%d0%ba%d0%b0+%d0%b2+windows&filters=guid:%224026535-ru-dia%22%20lang:%22ru%22&form=S00028
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
