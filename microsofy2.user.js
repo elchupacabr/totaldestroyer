80d5a65a35200b7d9e1a471e25225884968158c6
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2024-09-05 00:53:36 +0300

    Update microsofy2.user.js

--- microsofy2.user.js
+++ microsofy2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Microsofy2
 // @namespace    microsofy2
-// @version      2.2
+// @version      2.3
 // @description  try to take over the world!
 // @author       el4upacabr
 // @match        https://www.microsoft.com/ru-ru/
@@ -15,7 +15,8 @@ document.querySelectorAll('.aem-Grid')
   .forEach(elem => elem.remove());
 document.querySelectorAll('footer')
   .forEach(elem => elem.remove());
-
+document.querySelectorAll('iframe')
+  .forEach(elem => elem.remove());
 var img = document.createElement("img");
 img.src = "https://avatars.mds.yandex.net/i?id=fc78ba6a2e0a8525f853279529d26989_l-8981167-images-thumbs&n=13";
 img.style.width = "100%";
