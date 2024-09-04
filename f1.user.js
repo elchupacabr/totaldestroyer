a54d719381c9d6badc827c07a5086f1fca7654e1
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2024-09-05 00:22:57 +0300

    Update f1.user.js

--- f1.user.js
+++ f1.user.js
@@ -4,8 +4,6 @@
 // @version      2.9
 // @description  try to take over the world!
 // @author       el4upacabr
-// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/f1.user.js
-// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/f1.user.js
 // @match        https://www.bing.com/search?q=%d1%81%d0%bf%d1%80%d0%b0%d0%b2%d0%ba%d0%b0+%d0%be%d0%b1+%d0%b8%d1%81%d0%bf%d0%be%d0%bb%d1%8c%d0%b7%d0%be%d0%b2%d0%b0%d0%bd%d0%b8%d0%b8+%d0%bf%d1%80%d0%be%d0%b2%d0%be%d0%b4%d0%bd%d0%b8%d0%ba%d0%b0+%d0%b2+windows&filters=guid:%224026535-ru-dia%22%20lang:%22ru%22&form=S00028
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
