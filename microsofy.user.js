7782dc121fba8319ff2f618a2308683d5acf425d
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2024-09-04 23:19:59 +0300

    Update microsofy.user.js

--- microsofy.user.js
+++ microsofy.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://microsoft.com/*
+// @match        *://www.microsoft.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
 // ==/UserScript==
