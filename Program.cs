e0a7b13f16d6f830351bf0093312c1b8f828c2e5
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2024-11-08 17:56:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,61 +1,43 @@
 using System;
-using System.Collections.Generic;
-using System.Deployment.Internal;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace MyNamespace
 {
     class Program
     {
-
         static void Main()
         {
             while (true)
             {
                 Console.Write("Введите количество строк (n): ");
                 string n = Console.ReadLine();
-                Regex regex = new Regex(@"[^0-9\.\-]");
 
-                // Проверяем, является ли ввод числом
-                if (int.TryParse(n, out int number))
+                if (!int.TryParse(n, out int number) || number <= 0)
                 {
-                    Console.WriteLine("Введённые данные является числами ");
-                }
-                else
-                {
-                    Console.WriteLine("Введённые данные не являются числом.");
-                    Console.ReadKey();
-                    System.Environment.Exit(0);
+                    Console.WriteLine("Ошибка: введите положительное целое число для строк.");
+                    continue;
                 }
 
                 Console.Write("Введите количество столбцов (m): ");
                 string m = Console.ReadLine();
-                if (int.TryParse(m, out int number1))
+
+                if (!int.TryParse(m, out int number1) || number1 <= 0)
                 {
-                    Console.WriteLine("Введённые данные является числами ");
-                  
-                }
-                else
-                {
-                    Console.WriteLine("Введённые данные не являются числом.");
-                    Console.ReadKey();
-                    System.Environment.Exit(0);
+                    Console.WriteLine("Ошибка: введите положительное целое число для столбцов.");
+                    continue;
                 }
 
                 // Создание матрицы
                 double[,] matrix = new double[number, number1];
 
-                // Ввод значений матрицы
+                // Ввод значений матрицы с проверкой
                 for (int i = 0; i < number; i++)
                 {
                     for (int j = 0; j < number1; j++)
                     {
-                       //bool isValid = false;
-                        //do
-                       //{
+                        bool isValid = false;
+                        do
+                        {
                             Console.Write($"Введите элемент [{i}, {j}]: ");
                             string input = Console.ReadLine();
 
@@ -63,17 +45,16 @@ namespace MyNamespace
                             if (double.TryParse(input, out double value))
                             {
                                 matrix[i, j] = value;
-                                //isValid = true;
+                                isValid = true;
                             }
                             else
                             {
-                                Console.WriteLine("ошибка! некорректное заполнение");
-                                Console.ReadKey();
-                                System.Environment.Exit(0);
+                                Console.WriteLine("Ошибка: введите корректное число.");
                             }
-                        //} while (isValid);
+                        } while (!isValid);
                     }
                 }
+
                 // Преобразование матрицы в одномерный массив
                 double[] flatArray = new double[number * number1];
                 int k = 0;
@@ -84,6 +65,7 @@ namespace MyNamespace
                         flatArray[k++] = matrix[i, j];
                     }
                 }
+
                 // Сортировка массива
                 Array.Sort(flatArray);
 
@@ -93,10 +75,11 @@ namespace MyNamespace
                 {
                     for (int j = 0; j < number1; j++)
                     {
-                        matrix[j, i] = flatArray[k++];
+                        matrix[i, j] = flatArray[k++];
                     }
                 }
-                // Вывод результата
+
+                // Вывод отсортированной матрицы
                 Console.WriteLine("\nМатрица после сортировки:");
                 for (int i = 0; i < number; i++)
                 {
@@ -109,8 +92,6 @@ namespace MyNamespace
                 Console.WriteLine();
                 Console.ReadKey();
             }
-            
         }
-
     }
 }
