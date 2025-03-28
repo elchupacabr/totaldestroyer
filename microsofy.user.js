ff02b694067ecce0586988c44ff1b435a14b562f
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2025-03-28 23:39:31 +0300

    Update microsofy.user.js

--- microsofy.user.js
+++ microsofy.user.js
@@ -1,13 +1,18 @@
 // ==UserScript==
 // @name         Microsofy
 // @namespace    microsofy
-// @version      0.2
-// @description  try to take over the world!
+// @version      1.6
+// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/microsofy.user.js
+// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/microsofy.user.js
+// @description  skibidi hehehe
 // @author       el4upacabr
 // @match        https://www.microsoft.com/ru-ru/
 // @match        https://www.microsoft.com/ru-ru
 // @match        *://www.microsoft.com/*
+// @match        *://*.microsoft.com/*
 // @match        https://www.microsoft.com/ru-ru/*
+// @match        https://www.microsoft.com/uk-ua/*
+// @match        https://www.microsoft.com/en-us/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=microsoft.com
 // @grant        none
 // ==/UserScript==
