d4bc2e93f87909019c23666cdba338d9008e887c
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2025-03-28 23:27:25 +0300

    Update google_dead.user.js

--- google_dead.user.js
+++ google_dead.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         google dead
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      3.0
 // @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/refs/heads/main/google_dead.user.js
 // @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/refs/heads/main/google_dead.user.js
 // @description  try to take over the world!
@@ -15,8 +15,12 @@
 // @grant        none
 // ==/UserScript==
 
+//not worked хз почему
 document.querySelector("#SIvCob").lastElementChild.textContent = "орлеанский";
 document.querySelector("#SIvCob a").style.textDecoration = 'underline'
+//
+//==bonus
+document.querySelector('.pHiOh').textContent = 'яяяяяяяязь'
 document.querySelector('.lnXdpd').src = 'https://elchupacabr.github.io/zerotrust/back%20to%20undeground%20(1).png';
 document.querySelector('.lnXdpd').srcset = 'https://elchupacabr.github.io/zerotrust/back%20to%20undeground%20(1).png';
 document.querySelector('.lnXdpd').height = 142
@@ -25,5 +29,8 @@ document.querySelectorAll('.gNO89b')
   .forEach(elem => elem.remove());
 document.querySelectorAll('.RNmpXc')
   .forEach(elem => elem.remove());
+document.querySelectorAll('.FPdoLc.lJ9FBc')
+  .forEach(elem => elem.remove());
+// for USA and another countries except UA
 document.querySelector('.ktLKi').textContent = 'Наше третье десятилетие борьбы с запором';
 
