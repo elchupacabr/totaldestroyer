a0be9c4364103a5483e07b8dbb7e83810b313f50
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2026-01-25 06:21:13 +0300

    Update fix_na_gh.user.js

--- fix_na_gh.user.js
+++ fix_na_gh.user.js
@@ -1,3 +1,30 @@
-.cm-content{
+// ==UserScript==
+// @name         fix_na_gh
+// @namespace    https://tampermonkey.net/
+// @version      1.0
+// @description  Нихера се
+// @match        *://github.com/*
+// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/fix_na_gh.user.js
+// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/fix_na_gh.user.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+const style = document.createElement('style');
+style.textContent = `
+ 
+  .cm-content{
 background-color: #00000000 !important;
 }
+`;
+
+document.head.appendChild(style);
+
+
+
+})();
+
+   
