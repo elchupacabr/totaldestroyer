246949e1ae3eaaa9dd5b6edf591386eee2e864e7
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2026-01-25 05:13:46 +0300

    Update and rename fix_na_poisk_v_yandexe.user.js to fix_nihua_sebe.user.js

---
+++ fix_nihua_sebe.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         lol
+// @namespace    https://tampermonkey.net/
+// @version      1.0
+// @description  Нихера се
+// @match        *://*/*
+// @match        *://*
+// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/fix_nihua_sebe.user.js
+// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/fix_nihua_sebe.user.js
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+const style = document.createElement('style');
+style.textContent = `
+  input,
+  textarea,
+  input:focus,
+  textarea:focus,
+  input:active,
+  textarea:active {
+    color: #fff !important;
+    background-color: #00000000 !important;
+  }
+`;
+
+document.head.appendChild(style);
+
+
+
+})();
+
+   
