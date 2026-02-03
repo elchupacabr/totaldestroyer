81ae971b700b9b440057a4cc037b19f10fcd746b
nightmafia895 <90207912+elchupacabr@users.noreply.github.com>
2026-02-04 00:41:39 +0300

    Update fix_nihua_sebe.user.js

--- fix_nihua_sebe.user.js
+++ fix_nihua_sebe.user.js
@@ -1,57 +1,32 @@
 // ==UserScript==
-// @name         fix all sites (light mode)
+// @name         lol
 // @namespace    https://tampermonkey.net/
-// @version      1.1
+// @version      1.4
 // @description  Нихера се
-// @match        *://*
-// @match        *://*/*
+// @match        *://*.yandex.ru/*
+// @match        *://*.ya.ru/*
+// @match        *://*.yandex.com/*
+// @match        *://*.yandex.by/*
+// @match        *://*.yandex.kz/*
+// @match        *://*.yandex.uz/*
+// @match        *://*.yandex.ua/*
+// @match        *://*.yandex.az/*
+// @match        *://*.yandex.am/*
+// @match        *://*.yandex.ge/*
+// @match        *://*.yandex.md/*
+// @match        *://*.yandex.ee/*
+// @match        *://*.yandex.lv/*
+// @match        *://*.yandex.lt/*
+// @match        *://*.yandex.tj/*
+// @match        *://*.yandex.tm/*
+// @match        *://*.yandex.fr/*
+// @match        *://*.yandex.co.il/*
+// @match        *://*.yandex.com.tr/*
+// @match        *://*.youtube.com/*
+// @match        *://vk.com/*
 // @exclude        *://mail.yandex.ru/*
-// @exclude        *://yandex.ru/*
-// @exclude        *://ya.ru/*
-// @exclude        *://yandex.com/*
-// @exclude        *://yandex.by/*
-// @exclude        *://yandex.kz/*
-// @exclude        *://yandex.uz/*
-// @exclude        *://yandex.az/*
-// @exclude        *://yandex.am/*
-// @exclude        *://yandex.ge/*
-// @exclude        *://yandex.md/*
-// @exclude        *://yandex.ee/*
-// @exclude        *://yandex.lv/*
-// @exclude        *://yandex.lt/*
-// @exclude        *://yandex.tj/*
-// @exclude        *://yandex.tm/*
-// @exclude        *://yandex.fr/*
-// @exclude        *://yandex.co.il/*
-// @exclude        *://yandex.com.tr/*
-// @exclude        *://*.yandex.ru/*
-// @exclude        *://*.ya.ru/*
-// @exclude        *://*.yandex.com/*
-// @exclude        *://*.yandex.by/*
-// @exclude        *://*.yandex.kz/*
-// @exclude        *://*.yandex.uz/*
-// @exclude        *://*.yandex.az/*
-// @exclude        *://*.yandex.am/*
-// @exclude        *://*.yandex.ge/*
-// @exclude        *://*.yandex.md/*
-// @exclude        *://*.yandex.ee/*
-// @exclude        *://*.yandex.lv/*
-// @exclude        *://*.yandex.lt/*
-// @exclude        *://*.yandex.tj/*
-// @exclude        *://*.yandex.tm/*
-// @exclude        *://*.yandex.fr/*
-// @exclude        *://*.yandex.co.il/*
-// @exclude        *://*.yandex.com.tr/*
-// @exclude        *://*.youtube.com/*
-// @exclude        *://vk.com/*
-// @exclude        *://*.vk.com/*
-// @exclude        *://suno.com/*
-// @exclude      *://github.com/*
-// @exclude      *://*.soundcloud.com/*
-// @exclude      *://soundcloud.com/*
-// @exclude      *://*.sssu.ru/*
-// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/fix_all_sites-l.user.js
-// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/fix_all_sites-l.user.js
+// @updateURL    https://github.com/elchupacabr/totaldestroyer/raw/main/fix_nihua_sebe.user.js
+// @downloadURL  https://github.com/elchupacabr/totaldestroyer/raw/main/fix_nihua_sebe.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -67,10 +42,17 @@ style.textContent = `
   textarea:focus,
   input:active,
   textarea:active {
-    color: #000 !important;
+    color: #fff !important;
     background-color: #00000000 !important;
   }
-  
+.vkuiUnstyledTextField__host.PostInput__input--krFir.PostInputWithEmoji__input--ksk6C.vkuiText__host.vkuiText__sizeYCompact.vkuiTypography__host.vkuiRootComponent__host{
+background-color: #00000000 !important;
+color: #fff !important;
+}
+.ComposerInput__input.ConvoComposer__input.ComposerInput__input--fixed{
+background-color: #00000000 !important;
+color: #fff !important;
+}
 `;
 
 document.head.appendChild(style);
@@ -78,3 +60,5 @@ document.head.appendChild(style);
 
 
 })();
+
+   
